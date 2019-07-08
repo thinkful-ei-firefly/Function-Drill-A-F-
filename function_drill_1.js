6f6a7cf98937d00dbadeb2bb1364460dd03bc42b
Ahmed <ahmed.max7586@gmail.com>
2019-07-08 15:20:45 -0400

    function returned and loged out

--- function_drill_1.js
+++ function_drill_1.js
@@ -1,5 +1,6 @@
 function createGreeting(){
-	console.log('Hi, my name is ahmed and I\'m 29 years old ');
+	return 'Hi, my name is ahmed and I\'m 29 years old ' ;
 }
 
-createGreeting();
+const greeting1 = createGreeting();
+console.log(greeting1); 
