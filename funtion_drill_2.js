851dc3e6aed25954df6e82b8b3765681d6ba372a
Fabian Lema <fabianlemac@gmail.com>
2019-07-08 16:06:27 -0500

    Created function beyond

--- funtion_drill_2.js
+++ funtion_drill_2.js
@@ -5,3 +5,20 @@ function jedyName(fistName, lastName){
 }
 
 console.log(jedyName("Fabian", "Lema"));
+
+function beyond(num){
+	if (typeof num != "number"){
+		console.log("And beyond");
+	}else if (num>0){
+		console.log("To infinity");
+	}else if (num<0){
+		console.log("To negative infinity");
+	}else if (num==0){
+		console.log("Staying home");
+	}
+} 
+
+beyond("sdfsd");
+beyond(-234);
+beyond(3423);
+beyond(0);
