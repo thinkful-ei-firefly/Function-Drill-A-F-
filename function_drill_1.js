4d8d460ef51c12839d5acf5154fd689b3c0b4a05
Fabian Lema <fabianlemac@gmail.com>
2019-07-08 15:02:54 -0500

    Validate name and age defined

--- function_drill_1.js
+++ function_drill_1.js
@@ -6,17 +6,21 @@ function  getYearOfBirth(age){
 }
 
 function createGreeting(name, age){
+	if (!name || !age){
+		throw new Error('Arguments not valid');
+	}
     const yob = getYearOfBirth(age);
-
 	return `Hi, my name is ${name} and I'm ${age} years old. I was born in ${yob} ` ;
 }
 
 
 try {
-	const greeting1 = createGreeting("Fabian", -23);
+	const greeting1 = createGreeting("Fabian", 45);
 	console.log(greeting1);
-} catch(error){console.log(error.message)}
+} catch(error){
+	console.log(error.message)
+}
 // const greeting1 = createGreeting("Fabian", -23);
 
 
-console.log("hello")
+//console.log("hello")
